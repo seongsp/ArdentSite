7851ea6c718d74282e5d85ee08dede22b56c5871
Seong Su Park <sspark114@gmail.com>
2021-11-17 13:40:47 -0800

    Fixed bug where software-engineering quick link would not turn blue when active

--- src/components/Contact/QuickLinks/QuickLinks.jsx
+++ src/components/Contact/QuickLinks/QuickLinks.jsx
@@ -4,12 +4,13 @@ import { useLocation } from 'react-router-dom';
 import './QuickLinks.css';
 
 function QuickLinks() {
+    console.log(useLocation().pathname);
     return (
         <div id="quicklinks" className="contact-item">
             <hr className="contact-line" />
             <h2 className="contact-item-title size-24">QUICK LINKS</h2>
             <HashLink smooth to="/#" className={useLocation().pathname === "/" ? "link-active size-14" : "link size-14"}>Home</HashLink>
-            <HashLink smooth to="/software-engineering-remote-interns/#" className={useLocation().pathname === "/software-engineering-remote-interns size-14" ? "link-active" : "link size-14"}>Software Engineering Virtual Internship</HashLink>
+            <HashLink to="/software-engineering-remote-interns" className={useLocation().pathname === "/software-engineering-remote-interns" ? "link-active size-14" : "link size-14"}>Software Engineering Virtual Internship</HashLink>
             <HashLink smooth to="/#mission-section" className="link size-14">Mission</HashLink>
             <HashLink smooth to="/#about-section" className="link size-14">About</HashLink>
             <HashLink smooth to="/#portfolio-section" className="link size-14">Portfolio</HashLink>
