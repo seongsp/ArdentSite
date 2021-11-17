2c8924d1e411c1699c599e16eab20ae80b143751
Seong Su Park <sspark114@gmail.com>
2021-11-17 14:58:52 -0800

    SEAnnouncement section done

--- src/components/SoftwareInternPage/SEAnnouncement/SEAnnouncement.jsx
+++ src/components/SoftwareInternPage/SEAnnouncement/SEAnnouncement.jsx
@@ -6,6 +6,10 @@ function SEAnnouncement() {
         <div id="SEAnnouncement-section">
             <h1 className="title">SOFTWARE ENGINEERING</h1>
             <h1 className="title">VIRTUAL INTERNSHIP</h1>
+            <div id="SEAnnouncement-main">
+                <p className="size-24 bolder">As many internships have been suspended, or even cancelled, Ardent Labs aims to continue bringing quality internship programs to everyone at home during the crisis. During our 8-10 weeks virtual spring internship, interns will have the opportunity to learn modern web development tools by working on multiple milestone projects.</p>
+                <hr className="se-page-line" />
+            </div>
         </div>
     )
 }
